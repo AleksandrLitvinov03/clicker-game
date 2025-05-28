fb4e261e8395347e9d85e35d7d0a9347d1d901ae
AleksandrLitvinov03 <aleksandrlitvinov2003@gmail.com>
2025-05-28 08:55:36 +0300

    Nine

--- clicker-game/vite.config.ts
+++ clicker-game/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/clicker-game/', // This should match your repository name
+  base: '/', // Changed from '/clicker-game/' to '/' for Netlify
 })
